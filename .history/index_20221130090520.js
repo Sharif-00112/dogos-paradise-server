1a57bdec01b0bb184e3ae26dc480c96ee6e156c5
Sharif Hossain <it22@bengal.com.bd>
2022-11-30 14:14:33 +0600

    database changed

--- .history/index_20221130090520.js
+++ .history/index_20221130090520.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 
 //connecting database
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gddtk1o.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cny0fg3.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
